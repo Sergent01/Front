9e5756527b5ee757432221692a63cf71b973cef9
Léo Peyré <leo.peyre95@gmail.com>
2022-09-25 19:22:07 +0200

    Ajout de suppression BO et redic-rection vers /

--- src/components/header/HeaderBO/HeaderBO.js
+++ src/components/header/HeaderBO/HeaderBO.js
@@ -1,27 +1,35 @@
-import React from 'react';
+import React from "react";
 import { useRouter } from "next/router";
 
+import styles from "./HeaderBO.module.scss";
 
-import styles from './HeaderBO.module.scss';
-
-import Icon from "../../../../public/retour.png"
+import Icon from "../../../../public/retour.png";
 
 const HeaderBO = () => {
-    const router = useRouter();
-    const redirectTo = (path) => {
-        router.push({ pathname: `/` });
-      };
+  const router = useRouter();
+  const redirectTo = () => {
+    router.push({ pathname: `/` });
+  };
+  const logout = () => {
+    localStorage.removeItem("token");
+    redirectTo()
+  };
 
-    return (
-        <div className={styles.headerBO__main}>
-            <div className={styles.headerBO__title}>
-                <h2>Back Office</h2>
-            </div>
-            <div className={styles.headerBO__button}>
-                <img onClick={() => redirectTo()} src={Icon.src} alt=""/>
-            </div>
+  return (
+    <div className={styles.headerBO__main}>
+      <div className={styles.headerBO__title}>
+        <h2>Back Office</h2>
+      </div>
+      <div className={styles.headerBO__button}>
+        <div className={styles.headerBO__btn_logout}>
+          <p onClick={() => logout()}>DECONNEXION</p>
         </div>
-    );
+        <div className={styles.headerBO__btn_redirect}>
+          <img onClick={() => redirectTo()} src={Icon.src} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HeaderBO;
